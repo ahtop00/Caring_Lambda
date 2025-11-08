912aa870743bacd4038e49591ee409fa369ad41e
KYS <ehcl1027@gmail.com>
2025-11-08 13:36:14 +0900

    feat: Vertex AI (Gemini) API 호출 기능 추가

--- chatbot/config.py
+++ chatbot/config.py
@@ -11,6 +11,7 @@ class AppConfig:
         self.db_user = os.environ['DB_USER']
         self.db_password = os.environ['DB_PASSWORD']
         self.anthropic_api_key = os.environ.get('ANTHROPIC_API_KEY')
+        self.gcp_ssm_param_name = os.environ.get('GCP_SSM_PARAM_NAME')
 
         if not all([self.db_host, self.db_name, self.db_user, self.db_password]):
             msg = "데이터베이스 환경 변수가 하나 이상 누락되었습니다."
